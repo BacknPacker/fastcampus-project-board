8fbd01bca13ac6567153caa2a36a7581d86b166b
quedevel <kdh90817@naver.com>
2023-04-30 15:53:40 +0900

    :zap: update: #20 - 게시글 서비스 로직의 테스트와 골격 잡기
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제

---
+++ src/main/java/fastcampus/projectboard/dto/ArticleDto.java
@@ -0,0 +1,15 @@
+package fastcampus.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleDto(
+        String title,
+        String content,
+        String hashtag,
+        String createdBy,
+        LocalDateTime createdAt
+) {
+    public static ArticleDto of(String title, String content, String hashtag, String createdBy, LocalDateTime createdAt) {
+        return new ArticleDto(title,content,hashtag,createdBy,createdAt);
+    }
+}
