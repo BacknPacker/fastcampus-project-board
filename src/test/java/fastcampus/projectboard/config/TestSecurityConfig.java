fffb71b82e4a19f5ff3a165a0bcf658e2bf66245
quedevel <kdh90817@naver.com>
2023-05-06 20:11:43 +0900

    feat: #44 - 시큐리티 설정과 자바 대응 코드 구현

---
+++ src/test/java/fastcampus/projectboard/config/TestSecurityConfig.java
@@ -0,0 +1,33 @@
+package fastcampus.projectboard.config;
+
+import fastcampus.projectboard.domain.UserAccount;
+import fastcampus.projectboard.repository.UserAccountRepository;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+
+@Import(SecurityConfig.class)
+public class TestSecurityConfig {
+
+    @MockBean private UserAccountRepository userAccountRepository;
+
+    @BeforeTestMethod
+    public void securitySetUp(){
+        given(userAccountRepository.findById(anyString()))
+                .willReturn(Optional.of(UserAccount.of(
+                        "queTest",
+                        "pw",
+                        "que-test@email.com",
+                        "que-test",
+                        "test memo"
+                )));
+    }
+
+}
